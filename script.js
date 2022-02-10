c40e171ae601cb45980176a8320aebe55be268a0
astrotiz <tixi8922@gmail.com>
2022-02-10 23:58:13 +0100

    all work done!

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let computerCounter = 0;
 function computerPlay() {
     let computerSelection = choiseArray[Math.floor(Math.random()*choiseArray.length)];
     console.log("Computer choise is: " + computerSelection);
+    document.querySelector('#turn').textContent="Computer choise is: " + computerSelection + ". ";
     return computerSelection;
 }
 
@@ -44,21 +45,50 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-function game(roundNumber) {
-    playerCounter = 0; //reinitialize the counter
-    computerCounter = 0; //reinitialize the counter
-    for (let i = 0; i < roundNumber; i++) {
-        console.log(playRound(prompt("Write your choise: "), computerPlay()));
+function game(playerSelection) {
+    console.log(playRound(playerSelection, computerPlay()));
+    document.querySelector('#counter').textContent="Computer score: " + computerCounter + ". Your score: " + playerCounter + ".";
+    if (computerCounter == 5 || playerCounter == 5) {
+        let winner
+        if (playerCounter < computerCounter) {
+            winner = "The winner is: Computer! Better luck next time!"
+            new Audio("sounds\\lose.mp3").play()
+        } else if (playerCounter > computerCounter) {
+            winner = "The winner is: You! Congrats!"
+            new Audio("sounds\\win.mp3").play()
+        } else if (playerCounter === computerCounter) {
+            winner = "It's a draw! Let's play another game!"
+        }
+        console.log(winner) // declares the winner
+        document.querySelector('#winner').textContent=String(winner)
+        document.getElementById("btnRock").disabled = true;
+        document.getElementById("btnPaper").disabled = true;
+        document.getElementById("btnScissors").disabled = true;
     }
-    let winner
-    if (playerCounter < computerCounter) {
-        winner = "The winner is: Computer! Better luck next time"
-    } else if (playerCounter > computerCounter) {
-        winner = "The winner is: You! Congrats"
-    } else if (playerCounter === computerCounter) {
-        winner = "It's a draw!"
-    }
-    console.log(winner) // declares the winner
 }
 
-game(prompt("How many rounds do you want to play?"));
+function reset() {
+    playerCounter = 0; //reinitialize the counter
+    computerCounter = 0; //reinitialize the counter
+    document.querySelector('#turn').textContent="";
+    document.querySelector('#counter').textContent="";
+    document.querySelector('#winner').textContent="";
+    document.getElementById("btnRock").disabled = false;
+    document.getElementById("btnPaper").disabled = false;
+    document.getElementById("btnScissors").disabled = false;
+}
+
+function playSound(name) {
+    new Audio("sounds\\"+name+".mp3").play()
+}
+
+const btnRockEvent = document.querySelector('#btnRock').addEventListener('click', function() {game("Rock"); playSound("rock");});
+
+const btnPaperEvent = document.querySelector('#btnPaper').addEventListener('click', function() {game("Paper"); playSound("paper");});
+
+const btnScissorsEvent = document.querySelector('#btnScissors').addEventListener('click', function() {game("Scissors"); playSound("scissors");});
+
+const btnResetEvent = document.querySelector('#btnReset').addEventListener('click', function() {reset()});
+
+
+/* aggiungere suoni di sasso carta forbice alla pressione del tasto */
